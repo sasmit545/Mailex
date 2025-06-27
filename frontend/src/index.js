d316f80974b60b0a747b06e138538132d9bbab12
Sasmit Shashwat <140724096+sasmit545@users.noreply.github.com>
2025-06-27 17:03:32 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,13 +8,13 @@ import { FirebaseProvider } from './firebase_context';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjepY15gTpfNOcw4phBbtuYWFq31wiK_4",
-  authDomain: "react-bea4e.firebaseapp.com",
-  projectId: "react-bea4e",
-  storageBucket: "react-bea4e.firebasestorage.app",
-  messagingSenderId: "942228996414",
-  appId: "1:942228996414:web:b99617aab0d68b894b6c6f",
-  measurementId: "G-EKJEHH09SN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
